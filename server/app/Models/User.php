496737c34c2e4ab80f7780d99e1de16615fb7c8a
Mohamad-Rida-Chouman <shumanmohammadrida@gmail.com>
2023-08-09 23:32:22 +0300

    Server <> Added API for AuthController

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -2,18 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable  implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
     ];
@@ -43,10 +45,10 @@ class User extends Authenticatable
         return $this -> hasMany(Post::class);
     }
 
-    public function followers()
-    {
-        return $this -> belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
-    }
+    // public function followers()
+    // {
+    //     return $this -> belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    // }
 
     public function following()
     {
